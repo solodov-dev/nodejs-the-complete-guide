31fb7b6976dfa92fdcfeac2fb2d2d0809eefc216
Andrey Solodov <solodov.dev@gmail.com>
2021-02-09 10:33:14 +0300

    Working with SQL and sequelize

---
+++ 7-sql-introduction/app.js
@@ -0,0 +1,27 @@
+const path = require('path');
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(errorController.get404);
+
+sequelize
+  .sync()
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err));
+
+app.listen(3000);
