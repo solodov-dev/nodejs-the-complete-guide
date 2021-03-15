85b83a8eeaa066794df3f55a7e9d19ac72b643b3
Andrey Solodov <solodov.dev@gmail.com>
2021-03-15 15:11:11 +0300

    Lesson 16: sending emails

--- 10-authentication/controllers/auth.js
+++ 10-authentication/controllers/auth.js
@@ -1,5 +1,15 @@
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
+
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key: process.env.EMAIL_API_KEY,
+    },
+  })
+);
 
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
@@ -62,7 +72,16 @@ exports.postSignup = (req, res, next) => {
           });
           return user.save();
         })
-        .then((r) => res.redirect('/login'));
+        .then((r) => {
+          res.redirect('/login');
+          return transporter.sendMail({
+            to: email,
+            from: process.env.EMAIL_SENDER,
+            subject: 'Signup succeeded!',
+            html: '<h1>You successfully signed up!</h1>',
+          });
+        })
+        .catch((err) => console.log(err));
     })
     .catch((err) => console.log(err));
 };
